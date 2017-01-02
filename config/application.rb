61c7bbb926b293a91261b218293c62c6e5dce7bf
Mithrandier <andrew.prostakov@gmail.com>
2017-01-02 23:56:18 +0300

    Added Russian locale

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module TryRails5
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.default_locale = :ru
   end
 end
