364006e26b3dd8946c1542c5bfc89e5c550b57ce
Andrey Prostakov <andrey.prostakov@kandasoft.com>
2017-03-31 12:23:06 +0300

    Fixed AuthorsController spec

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -6,53 +6,39 @@ RSpec.describe AuthorsController do
     before { allow(Author).to receive(:by_names).and_return(authors) }
 
     it 'renders all authors by their names' do
-      get :index
-      expect(response).to render_template('index')
-      expect(assigns :authors).to eq authors
-    end
-  end
-
-  describe 'GET show' do
-    let(:author) do
-      build_stubbed(:author,
-        books: [build(:book, authors: [])]
-      )
-    end
-    before do
-      allow(Author).to receive(:find).with(author.id.to_s).and_return(author)
-    end
-
-    it 'shows author info' do
-      get :show, params: { id: author.id }
-      expect(response).to render_template('show')
-      expect(assigns :author).to eq author
+      get :index, xhr: true
+      expect(response).to be_success
+      expect(response.body).to eq ActiveModelSerializers::SerializableResource.new(authors).to_json
     end
   end
 
   describe 'POST create' do
-    context 'when parameters are valid' do
-      let(:author_params) { { name: 'The Author' } }
-
-      it 'creates Author and redirects to index' do
-        expect do
-          post :create, params: { author: author_params }
-        end.to change { Author.count }.by(1)
-        expect(response).to redirect_to authors_path
-        expect(assigns :author).to be_persisted
-        expect(assigns(:author).name).to eq 'The Author'
-      end
-    end
-
     context 'when parameters are invalid' do
       let(:author_params) { { name: '' } }
 
       it 'builds Author and shows form again' do
         expect do
-          post :create, params: { author: author_params }
+          post :create, params: { author: author_params }, xhr: true
         end.not_to change { Author.count }
-        expect(response).to render_template :new
-        expect(assigns :author).to be_new_record
-        expect(assigns :author).not_to be_valid
+        expect(response.status).to be 422
+        author = assigns :author
+        expect(author).to be_new_record
+        expect(response.body).to eq author.errors.to_json
+      end
+    end
+
+    context 'when parameters are valid' do
+      let(:author_params) { { name: 'The Author' } }
+
+      it 'creates Author and redirects to index' do
+        expect do
+          post :create, params: { author: author_params }, xhr: true
+        end.to change { Author.count }.by(1)
+        expect(response).to be_success
+        author = assigns :author
+        expect(author).to be_persisted
+        expect(author.name).to eq 'The Author'
+        expect(response.body).to eq ActiveModelSerializers::SerializableResource.new(author).to_json
       end
     end
   end
@@ -64,9 +50,11 @@ RSpec.describe AuthorsController do
       let(:author_params) { { name: '' } }
       it 'renders form again' do
         expect do
-          put :update, params: { id: author.id, author: author_params }
+          put :update, params: { id: author.id, author: author_params }, xhr: true
         end.not_to change { author.reload.name }
-        expect(response).to render_template('edit')
+        expect(response.status).to be 422
+        author = assigns :author
+        expect(response.body).to eq author.errors.to_json
       end
     end
 
@@ -74,9 +62,10 @@ RSpec.describe AuthorsController do
       let(:author_params) { { name: 'New name' } }
       it 'updates author and redirects to index' do
         expect do
-          put :update, params: { id: author.id, author: author_params }
+          put :update, params: { id: author.id, author: author_params }, xhr: true
         end.to change { author.reload.name }.to('New name')
-        expect(response).to redirect_to authors_path
+        expect(response).to be_success
+        expect(response.body).to eq ActiveModelSerializers::SerializableResource.new(author).to_json
       end
     end
   end
@@ -86,9 +75,9 @@ RSpec.describe AuthorsController do
 
     it 'removes author from database and redirects to index' do
       expect do
-        delete :destroy, params: { id: author.id }
+        delete :destroy, params: { id: author.id }, xhr: true
       end.to change { Author.count }.by(-1)
-      expect(response).to redirect_to authors_path
+      expect(response).to be_success
     end
   end
 end
