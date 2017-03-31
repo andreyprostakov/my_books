b473655ea36585c4ce9ef196fd6cc938623720b8
Andrey Prostakov <andrey.prostakov@kandasoft.com>
2017-03-31 12:27:21 +0300

    Fixed PublishersController spec

--- spec/controllers/publishers_controller_spec.rb
+++ spec/controllers/publishers_controller_spec.rb
@@ -6,36 +6,38 @@ RSpec.describe PublishersController do
     before { allow(Publisher).to receive(:by_names).and_return(publishers) }
 
     it 'renders all publishers' do
-      get :index
-      expect(response).to render_template('index')
-      expect(assigns :publishers).to eq publishers
+      get :index, xhr: true
+      expect(response).to be_success
+      expect(response.body).to eq ActiveModelSerializers::SerializableResource.new(publishers).to_json
     end
   end
 
   describe 'POST create' do
-    context 'when parameters are valid' do
-      let(:publisher_params) { { name: 'The Publisher' } }
-
-      it 'creates Publisher and redirects to index' do
-        expect do
-          post :create, params: { publisher: publisher_params }
-        end.to change { Publisher.count }.by(1)
-        expect(response).to redirect_to publishers_path
-        expect(assigns :publisher).to be_persisted
-        expect(assigns(:publisher).name).to eq 'The Publisher'
-      end
-    end
-
     context 'when parameters are invalid' do
       let(:publisher_params) { { name: '' } }
 
       it 'builds Publisher and shows form again' do
         expect do
-          post :create, params: { publisher: publisher_params }
+          post :create, params: { publisher: publisher_params }, xhr: true
         end.not_to change { Publisher.count }
-        expect(response).to render_template :new
-        expect(assigns :publisher).to be_new_record
-        expect(assigns :publisher).not_to be_valid
+        expect(response.status).to be 422
+        publisher = assigns :publisher
+        expect(publisher).to be_new_record
+        expect(publisher).not_to be_valid
+        expect(response.body).to eq publisher.errors.to_json
+      end
+    end
+
+    context 'when parameters are valid' do
+      let(:publisher_params) { { name: 'The Publisher' } }
+
+      it 'creates Publisher and redirects to index' do
+        expect do
+          post :create, params: { publisher: publisher_params }, xhr: true
+        end.to change { Publisher.count }.by(1)
+        expect(response).to be_success
+        expect(assigns :publisher).to be_persisted
+        expect(assigns(:publisher).name).to eq 'The Publisher'
       end
     end
   end
@@ -47,9 +49,11 @@ RSpec.describe PublishersController do
       let(:publisher_params) { { name: '' } }
       it 'renders form again' do
         expect do
-          put :update, params: { id: publisher.id, publisher: publisher_params }
+          put :update, params: { id: publisher.id, publisher: publisher_params }, xhr: true
         end.not_to change { publisher.reload.name }
-        expect(response).to render_template('edit')
+        publisher = assigns :publisher
+        expect(publisher).not_to be_valid
+        expect(response.body).to eq publisher.errors.to_json
       end
     end
 
@@ -57,9 +61,9 @@ RSpec.describe PublishersController do
       let(:publisher_params) { { name: 'New name' } }
       it 'updates publisher and redirects to index' do
         expect do
-          put :update, params: { id: publisher.id, publisher: publisher_params }
+          put :update, params: { id: publisher.id, publisher: publisher_params }, xhr: true
         end.to change { publisher.reload.name }.to('New name')
-        expect(response).to redirect_to publishers_path
+        expect(response).to be_success
       end
     end
   end
@@ -69,9 +73,9 @@ RSpec.describe PublishersController do
 
     it 'removes publisher from database and redirects to index' do
       expect do
-        delete :destroy, params: { id: publisher.id }
+        delete :destroy, params: { id: publisher.id }, xhr: true
       end.to change { Publisher.count }.by(-1)
-      expect(response).to redirect_to publishers_path
+      expect(response).to be_success
     end
   end
 end
